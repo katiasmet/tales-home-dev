594d2abe516a495d923d8d5579d5c85c161dc35d
katiasmet <katia.smet@hotmail.com>
2017-02-13 18:22:32 +0100

    :art: loader

--- src/js/components/Loading.jsx
+++ src/js/components/Loading.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Loading = () => {
   return (
     <section className='loading'>
-      <span className='loader-bullet loader-bullet-1'></span>
-      <span className='loader-bullet loader-bullet-2'></span>
-      <span className='loader-bullet loader-bullet-3'></span>
+      <div className='loader'>
+        <span className='loader-bullet loader-bullet-1'></span>
+        <span className='loader-bullet loader-bullet-2'></span>
+        <span className='loader-bullet loader-bullet-3'></span>
+      </div>
     </section>
   );
 };
